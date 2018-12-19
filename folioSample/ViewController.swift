17d181d54c07fb2e2187161b01418424c8eaa430
sachiko-kame <taiyousannsann2525sann@yahoo.co.jp>
2018-12-19 21:36:18 +0900

    Initial Commit

---
+++ folioSample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  folioSample
+//
+//  Created by 水野祥子 on 2018/12/19.
+//  Copyright © 2018年 sachiko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
