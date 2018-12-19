92c6f4209be39ce5795e76d170df37b506ba883e
sachiko-kame <taiyousannsann2525sann@yahoo.co.jp>
2018-12-19 23:48:33 +0900

    epub表示

--- folioSample/ViewController.swift
+++ folioSample/ViewController.swift
@@ -7,14 +7,36 @@
 //
 
 import UIKit
+import FolioReaderKit
 
 class ViewController: UIViewController {
-
+    var mainpath:String = ""
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-       
+        
+        let musicUrl = NSURL(string: "https://progit2.s3.amazonaws.com/ja/2016-02-11-8cb2f/progit-ja.1016.epub")
+        if let url = musicUrl {
+            let epubData = NSData(contentsOf: url as URL)
+            
+            let dataName = "FlowingExample3.epub"
+            
+            let documentsPath = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)[0]
+            
+            if let data = epubData {
+                self.mainpath = "\(documentsPath)/\(dataName)"
+                data.write(toFile: self.mainpath, atomically: true)
+            }
+        }
     }
-
-
+    
+    @IBAction func tap(_ sender: Any) {
+        let config = FolioReaderConfig(withIdentifier: "")
+        config.scrollDirection = .vertical
+        config.shouldHideNavigationOnTap = false
+//        let bookPath = Bundle.main.path(forResource: "FlowingExample3", ofType: "epub")
+        let folioReader = FolioReader()
+        folioReader.presentReader(parentViewController: self, withEpubPath: self.mainpath, andConfig: config)
+    }
 }
 
